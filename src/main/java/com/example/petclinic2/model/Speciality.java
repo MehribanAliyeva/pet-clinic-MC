62294c9ed7e8fbd70009c183ba1194884ac7d68d
MehribanAliyeva <lvmehriban@gmail.com>
2022-06-28 10:14:31 +0400

    created PetTypeMapService

---
+++ src/main/java/com/example/petclinic2/model/Speciality.java
@@ -0,0 +1,15 @@
+package com.example.petclinic2.model;
+
+import java.io.Serializable;
+
+public class Speciality extends BaseEntity implements Serializable {
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
---
+++ src/main/java/com/example/petclinic2/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package com.example.petclinic2.services;
+
+import com.example.petclinic2.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType,Long> {
+}
